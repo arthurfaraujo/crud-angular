b6e1712a3a0ecb452df6f3c18b31d97f30cf8181
arthur <arthurfelipe5567@gmail.com>
2024-04-19 18:42:57 -0300

    fix: change project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'crud-angular-spring' title`, () => {
+  it(`should have the 'crud-angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('crud-angular-spring');
+    expect(app.title).toEqual('crud-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, crud-angular-spring');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, crud-angular');
   });
 });
