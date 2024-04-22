0af8de52825eb893a5ec4f256e70c79f3c58b34d
arthur <arthurfelipe5567@gmail.com>
2024-04-22 14:48:51 -0300

    feat: create error dialog

---
+++ src/app/shared/error-dialog/error-dialog.component.ts
@@ -0,0 +1,23 @@
+import { Component, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogModule,
+} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-error-dialog',
+  standalone: true,
+  imports: [MatDialogModule,
+    MatButtonModule
+  ],
+  templateUrl: './error-dialog.component.html',
+  styleUrl: './error-dialog.component.scss',
+})
+export class ErrorDialogComponent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: string,
+    public dialog: MatDialog
+  ) {}
+}
