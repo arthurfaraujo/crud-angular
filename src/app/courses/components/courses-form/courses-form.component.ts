f7b5a67a6d995797ca0b72d04bcd319a3f795ada
arthur <arthurfelipe5567@gmail.com>
2024-04-27 13:03:24 -0300

    feat: add new courses-form component

---
+++ src/app/courses/components/courses-form/courses-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courses-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './courses-form.component.html',
+  styleUrl: './courses-form.component.scss'
+})
+export class CoursesFormComponent {
+
+}
