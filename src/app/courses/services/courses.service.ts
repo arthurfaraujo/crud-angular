330727eeb58350b683feaefe19ec6d509cc2f0f1
arthur <arthurfelipe5567@gmail.com>
2024-04-29 01:09:54 -0300

    feat: add

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -14,17 +14,29 @@ export class CoursesService {
 
   getAllCourses() {
     return this.httpClient.get<Course[]>(this.API).pipe(
-      first(),
+      first()
       // delay(5000),
       // tap((courses) => console.log(courses))
     );
   }
 
-  createCourse(course: Partial<Course>) {
+  save(course: Partial<Course>) {
+    return course._id ? this.update(course) : this.create(course);
+  }
+
+  private create(course: Partial<Course>) {
     return this.httpClient.post<Course>(this.API, course);
+  }
+
+  private update(course: Partial<Course>) {
+    return this.httpClient.put<Course>(`${this.API}/${course._id}`, course);
   }
 
   deleteCourse(id: number) {
     return this.httpClient.delete(`${this.API}/${id}`);
+  }
+
+  getById(id: string) {
+    return this.httpClient.get<Course>(`${this.API}/${id}`);
   }
 }
