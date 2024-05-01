61218e5b85560d909214d871517c60008f4c7333
arthur <arthurfelipe5567@gmail.com>
2024-05-01 16:23:46 -0300

    feat: create a confirmation dialog component

---
+++ src/app/shared/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,21 @@
+import { Component, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule],
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrl: './confirmation-dialog.component.scss',
+})
+export class ConfirmationDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: string
+  ) {}
+
+  onConfirm(result: boolean): void {
+    this.dialogRef.close(result);
+  }
+}
